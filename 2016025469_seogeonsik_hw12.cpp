369225ed62cb44bea9d92a82d3808e60d7bacd23
geonsikSeo <iqeq2328@gmail.com>
2017-01-03 10:47:03 +0900

    test commit zz

---
+++ 2016025469_seogeonsik_hw12.cpp
@@ -0,0 +1,14 @@
+/*#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float input;
+	printf("Input number : ");
+	scanf("%f", &input);
+
+	printf("%f\n%f\n%f\n%f\n%f", sqrt(input),log10(input),
+	sin(input)
+	,cos(input)
+	,tan(input));
+
+}*/
